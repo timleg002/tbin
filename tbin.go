d526d09574f955f871ad71c03f8fde176adcd974
Timotej Leginus <timko@zs.habovka.sk>
2020-11-10 01:01:01 +0100

    some json decoding

--- tbin.go
+++ tbin.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -8,7 +9,7 @@ import (
 
 //Constants
 const (
-	LISTEN_ADDR_INIT = "/init"
+	LISTEN_ADDR_INIT   = "/init"
 	LISTEN_ADDR_SUBMIT = "/submit_paste"
 )
 
@@ -28,11 +29,11 @@ func httpinit() *http.ServeMux {
 	return mux
 }
 
-func httphandle(url string){
-	http.HandleFunc(url, nil)//for now
+func httphandle(url string) {
+	http.HandleFunc(url, nil) //for now
 }
 
-func httplisten(){
+func httplisten() {
 
 }
 
@@ -41,11 +42,46 @@ func siteindexhandler(w http.ResponseWriter, req *http.Request) {
 	_, _ = fmt.Fprintf(w, "%v", string(content))
 }
 
-func submitpastehandler(w http.ResponseWriter, req *http.Request) {//POST
+func submitpastehandler(w http.ResponseWriter, req *http.Request) { //POST
 	err := req.ParseForm()
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println(req.PostForm.Get("paste"))//type name not label
+	fmt.Println(req.PostForm.Get("paste")) //type name not label
+	pasteread(0)
 }
 
+type pastefmt struct { //no id cause count of array is the ID
+	Text   string `json:"text"` //this was the error um...
+	Author string `json:"author"`
+	Link   string `json:"link"`
+}
+
+func pasteread(id int) {
+	bytecontent, err := ioutil.ReadFile("pastes.json")
+	if err != nil {
+		fmt.Println(err)
+	}
+	var pastes []pastefmt
+	err2 := json.Unmarshal(bytecontent, &pastes)
+	if err2 != nil {
+		fmt.Println(err2)
+	}
+	fmt.Println(pastes[id].Author)
+}
+
+func pastewrite(id int, paste pastefmt) {
+	bytecontent, err := ioutil.ReadFile("pastes.json")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	npaste, err2 := json.Marshal(paste)
+	if err2 != nil {
+		fmt.Println(err2)
+	}
+
+	bytecontent = append(bytecontent, npaste...) //unpacking slice
+
+	//TODO pastewrite() method,
+}
